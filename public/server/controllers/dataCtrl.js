a518a8e418ef2eb6250827f0eac469d06f302a3d
jimmurphyiv <jim.murphyiv@gmail.com>
2020-06-26 13:22:45 -0500

    made data

---
+++ public/server/controllers/dataCtrl.js
@@ -0,0 +1,107 @@
+const campG = [
+
+    {
+        id: 1,
+        name: 'Pacfic City',
+        location: 'Cloverdale, OR',
+        hookups: true
+
+    },
+
+
+    {
+        id: 2,
+        name: 'Whalers Rest',
+        location: 'Newport, OR',
+        hookups: true
+        
+    },
+
+    {
+        id: 3,
+        name: 'South Jetty',
+        location: 'Florence, OR',
+        hookups: true
+        
+    },
+
+
+    {
+        id: 4,
+        name: 'Russian River',
+        location: 'Cloverdale, CA',
+        hookups: true
+        
+    },
+
+
+    {
+        id: 5,
+        name: 'San Benito Reserve',
+        location: 'Pacines, CA',
+        hookups: true
+        
+    },
+
+
+    {
+        id: 6,
+        name: 'Soledad Canyon',
+        location: 'Acton, CA',
+        hookups: true
+        
+    },
+
+    {
+        id: 7,
+        name: 'Willderness Lakes',
+        location: 'Hemet, CA',
+        hookups: true
+        
+    },
+
+
+    {
+        id: 8,
+        name: 'Palm Springs Resort',
+        location: 'Palm Desert, CA',
+        hookups: true
+        
+    },
+
+
+    {
+        id: 9,
+        name: 'Verde Valley',
+        location: 'Verde Valley, AZ',
+        hookups: true
+    
+    },
+
+
+    {
+        id: 10,
+        name: 'Indian Lakes',
+        location: 'Batesville, IN',
+        hookups: true
+        
+    },
+
+
+    {
+        id: 11,
+        name: 'Diamond Caverns',
+        location: 'Park City, KY',
+        hookups: true
+        
+    },
+
+
+    {
+        id: 12,
+        name: 'Wilmington RV Resort',
+        location: 'Wilmington, OH',
+        hookups: true
+        
+    }
+]
