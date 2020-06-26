fdc42b2a1324fe7c0fa13ca663a87aca8fda4d38
jimmurphyiv <jim.murphyiv@gmail.com>
2020-06-26 16:33:30 -0500

    updated file path

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const dataCtrl = require('./controllers/dataCtrl');
-const woodsCtrl = require('./controllers/woodsCtrl');
-const truckCtrl = require('./controllers/truckCtrl');
+// const dataCtrl = require('./controllers/dataCtrl');
+// const woodsCtrl = require('./controllers/woodsCtrl');
+// const truckCtrl = require('./controllers/truckCtrl');
 const app = express();
 
 app.use(express.json());
