51c1dee2518e3461feaea833c012fd3388a43508
jimmurphyiv <jim.murphyiv@gmail.com>
2020-06-29 13:47:50 -0500

    update

--- src/Components/Truck.js
+++ src/Components/Truck.js
@@ -32,18 +32,21 @@ render(){
         <div>
             
             <img src={this.props.oneCampG.img} alt={this.props.oneCampG.name} />
-            {this.state.updateReview}
+            {this.state.updateReview ?(
        
-      <div>
-            <input value={this.state.reviewInput}
-                    onChange={e => this.handleUpdate(e.target.value)} />
+            <div>
+                <input value={this.state.reviewInput}
+                        onChange={e => this.handleUpdate(e.target.value)} />
 
-            <button onClick={() => this.handleUpdate(this.props.oneCampG.id)}>Submit</button>
-        </div>
+                <button onClick={() => this.handleUpdate(this.props.oneCampG.id)}>Submit</button>
+            </div> ) : (
 
-            <p>{this.props.oneCampG.review}</p>
-            <button onClick={this.reviewToggle}>Edit Review</button>
-
+                <div>
+                    <p>{this.props.oneCampG.review}</p>
+                    <button onClick={this.reviewToggle}>Edit Review</button>
+                </div>    
+            )}
+            <button onClick={this.props.deleteCampG(this.props.oneCampG.id)}>Delete</button>       
         </div>
 
         )
